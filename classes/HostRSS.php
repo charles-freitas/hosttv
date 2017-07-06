7e2f12c1a6250c06de3871221574f39124006f4b
Charles Cesar Magno de Freitas <ccmfreitas@gmail.com>
2017-07-06 15:31:24 -0300

    Initial Commit 2

---
+++ classes/HostRSS.php
@@ -0,0 +1,69 @@
+<?php
+
+class HostRSS {
+
+   // Atributos Privados
+   private $RssUrl;
+   private $rss;
+   private $TypeFeed;
+   private $ArrayFeed = array();
+
+   // Método Construtor
+   public function __construct($url) {
+      $this->RssUrl = $url;
+      $this->rss = simplexml_load_file($url);
+   }
+
+   // Método de Define o valor do atributo -> $RssUrl
+   public function setRssUrl($url) {
+      $this->RssUrl = $url;
+      $this->rss = simplexml_load_file($url);
+   }
+
+   // Método de Define o valor do atributo -> $TypeFeed
+   public function setTypeFeed($feedtype) {
+      $this->TypeFeed = $feedtype;
+   }
+
+   // Método de Retorna o valor do atributo -> $RssUrl
+   public function getRssUrl() {
+      return $this->RssUrl;
+   }
+
+   // Método de Retorna o valor do atributo -> $TypeFeed
+   public function getTypFeed() {
+      return $this->TypeFeed;
+   }
+
+   // Método para montar Array com o feed selecionado
+   public function setArrayFeed() {
+      $this->ArrayFeed = array();
+      foreach ($this->rss->channel->item as $itemArray) {
+         if ($itemArray->category == $this->TypeFeed) {
+            array_push($this->ArrayFeed, Array(
+                           "category" => $itemArray->category,
+                           "guid" => $itemArray->guid,
+                           "title" => $itemArray->title,
+                           "description" => $itemArray->description));
+         };
+      }
+   }
+
+   // Método ....
+   public function getElemArrayFeed($fieldName) {
+      //foreach ($this->ArrayFeed as $var) {
+      //   echo $var[$fieldName]."<br/><br/>";
+      return Array($this->ArrayFeed[$fieldName]);
+      //}
+   }
+
+   public function getArrayFeed2(){
+      foreach ( $this->ArrayFeed as $var ) {
+         echo $var['category']."<br/>".$var['title']."<br/>".$var['guid']."<br/>".$var['description']."<br/><br/><br/>";
+      }
+      //return var_dump($this->ArrayFeed);
+   }
+
+} // Fim da classe
+
+?>
